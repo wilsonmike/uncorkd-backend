c8db71ef3f7e48501def0d3aff78060c949be33c
Danielle Lucaj <danielle.lucaj@gmail.com>
2020-12-08 16:09:11 -0500

    added flavor endpoint

--- bourbon.api.js
+++ bourbon.api.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bourbon = express.Router();
 const pool = require("./connection");
 
+//For trending
 bourbon.get("/trending", (req, res) => {
   let query = `SELECT * FROM bourbon WHERE description LIKE '%vanilla%' LIMIT 10;`;
   pool.query(query).then((response) => {
@@ -10,17 +11,25 @@ bourbon.get("/trending", (req, res) => {
   });
 });
 
+//For search bar
 bourbon.get("/bourbons", (req, res) => {
   let term = req.query.term;
   console.log(term);
-  let query = `SELECT * FROM bourbon WHERE brand LIKE '${term}%';`;
+  let query = `SELECT * FROM bourbon WHERE UPPER(brand) LIKE UPPER('%${term}%');`;
   pool.query(query).then((response) => {
     res.json(response.rows);
   });
 });
 
-// bourbon.get("/", (req, res, searchTerm)=>{
-//   let query = `SELECT * FROM bourbon WHERE brand LIKE ${searchTerm}`
-// })
+//Train to flavor town
+
+bourbon.get("/flavor", (req, res) => {
+  let flavor = req.query.flavor;
+  console.log(flavor);
+  let query = `SELECT * FROM bourbon WHERE UPPER(description) LIKE UPPER('%${flavor}%') LIMIT 10`;
+  pool.query(query).then((response) => {
+    res.json(response.rows);
+  });
+});
 
 module.exports = bourbon;
